d1ee9e91ed7ff7c5366e153a536a769231bc765d
VitorMartin <vitor.martin.simoni@gmail.com>
2020-03-02 11:32:39 -0300

    Meu Primeiro Programa

---
+++ Linguagens1_Projetos/Pratica01/Aula/MeuPrograma.java
@@ -0,0 +1,9 @@
+class MeuPrograma{
+    public static void main (String[] args){
+        // Mensagens de saida do sistema
+        System.out.println("Minha primeira aplicacao Java!");
+        System.out.println("Ola Mundo!");
+        System.out.println("Estou programando no modo Conan.");
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 18000509_VitorMartin
+
 Repositório para a matéria Linguagens 1
