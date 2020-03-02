043049998973f49424ff3a8f08c94403aef6d082
VitorMartin <vitor.martin.simoni@gmail.com>
2020-03-02 13:02:05 -0300

    VSCode project

---
+++ Linguagens1_Projetos/Pratica01/Aula/TesteVSCode.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TesteVSCode {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ola Mundo do VSCode!");
+        String nome;
+        nome = scanner.nextLine();
+        System.out.println(nome);
+    }
+}
