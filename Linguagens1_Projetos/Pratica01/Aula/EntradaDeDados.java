4c22a525297c85dfb68199dee5732abc725388fc
VitorMartin <vitor.martin.simoni@gmail.com>
2020-03-02 12:13:51 -0300

    Input de int

--- Linguagens1_Projetos/Pratica01/Aula/EntradaDeDados.java
+++ Linguagens1_Projetos/Pratica01/Aula/EntradaDeDados.java
@@ -8,5 +8,12 @@ public class EntradaDeDados {
         System.out.print("Informe seu nome: ");
         nome = scanner.nextLine();
         System.out.println("Ola, " + nome);
+
+        int n1, n2;
+        System.out.println("Informe dois numeros inteiros, separados por [ENTER]: ");
+        n1 = scanner.nextInt();
+        n2 = scanner.nextInt();
+
+        System.out.println("Soma: " + (n1 + n2));
     }    
 }
