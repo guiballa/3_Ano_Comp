d3c9dc4123c2b9920524da2e30435e666a6986ec
VitorMartin <vitor.martin.simoni@gmail.com>
2020-03-03 12:35:29 -0300

    Directories

--- Linguagens1_Projetos/Pratica01/Aula/EntradaDeDados/EntradaDeDados.java
+++ Linguagens1_Projetos/Pratica01/Aula/EntradaDeDados/EntradaDeDados.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 
 public class EntradaDeDados {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
         String nome;
         System.out.print("Informe seu nome: ");
         nome = scanner.nextLine();
