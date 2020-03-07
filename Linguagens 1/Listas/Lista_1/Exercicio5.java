6956721eaff68fd2b619558246c51ed4d2b13793
VitorMartin <vitor.martin.simoni@gmail.com>
2020-03-07 18:49:41 -0300

    Ling1 Ex 5

--- Linguagens 1/Listas/Lista_1/Exercicio5.java
+++ Linguagens 1/Listas/Lista_1/Exercicio5.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Exercicio5{
-     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num;
-        int f1 = 0; 
-        int f2 = 1; 
-        int f3;
-        int i;
-        //Entrada de dados
-        System.out.println("Digite o numero desejado da sequencia: ");
-        num = scanner.nextInt();
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int num;
+    long fib;
+    //Entrada de dados
+    System.out.println("Digite o numero desejado da sequencia (maximo 92): ");
+    num = scanner.nextInt();
 
-        //Fibonacci
-            for(i = 1; i < num; i++){
-                f3 = f2 + f1;
-                f1 = f2;
-                f2 = f3;
-            }
-            System.out.println(" " + f2);
-     }
+    fib = Math.round(Math.pow((Math.sqrt(5) + 1) / 2, num) / Math.sqrt(5));
+
+    System.out.println("O " + num + ". numero de Fibonacci eh: " + fib + ".");
     }
+}
